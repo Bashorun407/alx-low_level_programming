553f6239c32f17dc4f2c96c5a246ca16b9a78bca
oluwaseyi <boboladvat@gmail.com>
2022-07-13 14:18:20 -0700

    Newly_updated_Task10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,29 +5,20 @@
  */
 int _atoi(char *s)
 {
-	int i, count, num, hold;
+	int i, count, num;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			count++;
-			num = s[i] - '0';
-			if (s[i + 1] >= '0' && s[i + 1] <= '9')
-			{
-				for (j = i + 1; (s[j] >= '0' && s[j] <= '9') && s[j] != ' '; j++)
-				{
-					hold = s[j] - 0;
-					num * 10;
-					num + s[j];
-				}
-			}
-			if (s[i - 1] == '-')
-				-1 * num;
-			return (num);
+			num = s[i];
+			return (num); /*returning the value of the number gotten*/
 		}
+		else
+			continue;
 	}
 	if (count == 0)
-		return (0);
+		return (0); /*return 0 if there is no number in the string*/
 }
 
