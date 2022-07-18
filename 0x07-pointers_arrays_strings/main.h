36b6228b2ed98cb28a346eb6e051b4d94b49c6cb
oluwaseyi <boboladvat@gmail.com>
2022-07-18 12:47:53 -0700

    Uploading_Task6_to_Task9

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -19,3 +19,15 @@ char *_strpbrk(char *s, char *accept);
 /* 5 a function that locates a substring*/
 char *_strstr(char *haystack, char *needle);
 
+/* 6 a function that prints the chessboard*/
+void print_chessboard(char (*a)[8]);
+
+/* 7 a function that prints the sum of the two diagonals of a square matrix of integers*/
+void print_diagsums(int *a, int size);
+
+/* 8 a function that sets the value of a pointer to a char*/
+void set_string(char **s, char *to);
+
+/* 9 to create a file that contains the password for the 'crackme2' executable*/
+/*the file created is: 101-crackme_password which contains the password :abc123*/
+
