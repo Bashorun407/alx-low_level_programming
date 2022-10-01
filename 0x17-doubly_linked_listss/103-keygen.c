95de87a66e943f874d0cfe8d947631eae0510dd1
olusheyi <olusheyi@gmail.com>
2022-10-01 11:16:02 -0700

    committing 0x17-doubly_linked_listss directory which contains the same files which should be in the 0x17-doubly_linked_lists directory in the remote github repository; but is empty. this implies that 0x17-doubly_linked_listss stands as a substitute for 0x17-doubly_linked_lists directory in the remote repository

---
+++ 0x17-doubly_linked_listss/103-keygen.c
@@ -0,0 +1,6 @@
+Write a keygen for crackme5.
+
+Usage of the crackme: ./crackme5 username key
+The crackme will segfault if you do not enter the correct key for the user
+Usage for your keygen: ./keygen5 username
+Your keygen should print a valid key for the username
